4d3d62a13c6d957d59882f93e6a36a25dd3343e4
dlrjsgh367 <dlrjsgh367@gmail.com>
2022-12-22 17:50:46 +0900

    main.py로 실행했을땐 print와 log가 번갈아 가면서 출력되는데, docker-compose 는 log 출력이 다 끝나고 print가 한번에 출력됨. docker-compose도 main.py 처럼 나오도록 바꾸려고 시도중

--- main.py
+++ main.py
@@ -30,14 +30,13 @@ def main():
     root_logger.addHandler(f_handler)
 
     # 현재 시점에서 최신 영화 리스트를 받아서 저장하고 가져오기
-    # logging("네이버 최근영화목록을 가져오고 있습니다.")
     print("네이버 최근영화목록을 가져오고 있습니다.")
     mcode_list = parsing_mcode_list()
+    
     # 최신 영화 리스트에서 영화마다 리뷰를 받아서 저장하기
     for mcode in mcode_list:
-        # logging(f"{mcode}을 파싱하고 있습니다.")
         print(f"{mcode}을 파싱하고 있습니다.")
-        parsing_reviews(mcode) 
+        parsing_reviews(mcode)  
 
 if __name__ == "__main__": #국룰.
     main()
