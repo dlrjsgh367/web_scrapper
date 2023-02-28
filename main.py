8ebb3c969c0ead8160c40dd4288ce1947adaf42e
dlrjsgh367 <dlrjsgh367@gmail.com>
2023-02-28 12:34:00 +0900

    검색 결과값들을 txt 파일로 보기좋게 저장함. 아직 남은 2개가 있는데 IPC, 특허설명임.

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
 import requests 
+
 from bs4 import BeautifulSoup
+
 import xml
+import xml.etree.ElementTree as ET
 
-from urllib import parse
+def sort(a, b, c, d, e, f, g, h, i, j, k):
 
-def search_result(type, Keyword):
-    # result_list = []
-    # url 주소
+    a_fix = '<root>{}</root>'.format(a)
+    a_content = ET.fromstring(a_fix).text
+
+    b_fix = '<root>{}</root>'.format(b)
+    b_content = ET.fromstring(b_fix).text
+    
+    c_fix = '<root>{}</root>'.format(c)
+    c_content = ET.fromstring(c_fix).text
+
+    d_fix = '<root>{}</root>'.format(d)
+    d_content = ET.fromstring(d_fix).text
+
+    e_fix = '<root>{}</root>'.format(e)
+    e_content = ET.fromstring(e_fix).text
+
+    f_fix = '<root>{}</root>'.format(f)
+    f_content = ET.fromstring(f_fix).text
+
+    g_fix = '<root>{}</root>'.format(g)
+    g_content = ET.fromstring(g_fix).text
+
+    h_fix = '<root>{}</root>'.format(h)
+    h_content = ET.fromstring(h_fix).text
+
+    i_fix = '<root>{}</root>'.format(i)
+    i_content = ET.fromstring(i_fix).text
+
+    j_fix = '<root>{}</root>'.format(j)
+    j_content = ET.fromstring(j_fix).text
+
+    k_fix = '<root>{}</root>'.format(k)
+    k_content = ET.fromstring(k_fix).text
+    
+    result = f"특허명 : {a_content}", f"IPC : {b_content}", f"출원인 : {c_content}", f"출원번호 : {d_content}", f"출원일자 : {e_content}", f"등록번호 : {f_content}", f"등록일자 : {g_content}", f"공개번호 : {h_content}", f"공개일자 : {i_content}", f"대리인 : {j_content}", f"발명자 : {k_content}", "\n"
+    return result
+
+
+# category : 검색유형
+# row : row개의 검색결과를 가져옴
+# Keyword : 검색어 입력(검색어 + 검색어 도 됨)
+def search_result(category, row, Keyword):
+    # 결과값을 담아놓는 리스트
+    result_list = []
+    
+    # url 
     url = "http://kportal.kipris.or.kr/kportal/resulta.do"
 
-    # header 값
+    # header
     headers = {
         "Accept": "application/xml, text/xml, */*; q=0.01",
         "Accept-Encoding": "gzip, deflate",
@@ -23,10 +68,11 @@ def search_result(type, Keyword):
         "Referer": "http://kportal.kipris.or.kr/kportal/search/total_search.do",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest"
-    }
-    # data 값
+        }
+    
+    # data 
     data = {
-    "next": f"{type}",
+    "next": f"{category}",
     # patentList
     # designList
     # trademarkList
@@ -36,7 +82,7 @@ def search_result(type, Keyword):
     "FROM": "SEARCH",
     "searchInTransKorToEng": "N",
     "searchInTransEngToKor": "N",
-    "row": "5",
+    "row": f"{row}",
     "page": "1",
     "queryText": f"{Keyword}",
     "expression": f"{Keyword}",
@@ -44,25 +90,40 @@ def search_result(type, Keyword):
 
     # response
     res = requests.post(url, headers=headers, data=data)
-
-    soup = BeautifulSoup(res.text, "html.parser")
+    
+    soup = BeautifulSoup(res.text, "lxml")
+    
     result = soup.find_all("article")
+
+    with open("search_result.xml", "w", encoding='utf8') as f:
+        f.write(str(result))
+
     for item in result:
-        print(f"특허명   : {item.find('tlv').text}")
-        print(f"IPC      : {item.find('ipv').text}")
-        # print(f"출원인   : {item.find('apv').text}")
-        # print(f"출원번호 : {item.find('vdkvgwkey').text}")
-        # print(f"출원일자 : {item.find('adv').text}")
-        # print(f"등록번호 : {item.find('gnv').text}")
-        # print(f"등록일자 : {item.find('gdv').text}")
-        # print(f"공개번호 : {item.find('onv').text}")
-        # print(f"공개일자 : {item.find('odv').text}")
-        # print(f"대리인   : {item.find('agv').text}")
-        # print(f"발명자   : {item.find('inv').text}")
-        print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-    # with open("search_result.xml", "w", encoding='utf8') as f:
-    #     f.write(str(result))
+        tlv = item.find('tlv').text # 특허명
+        ipv = item.select_one('a').text # IPC
+        apv = item.find('apv').text # 출원인
+        vdkvgwkey = item.find('vdkvgwkey').text # 출원번호
+        adv = item.find('adv').text # 출원일자
+        gnv = item.find('gnv').text # 등록번호
+        gdv = item.find('gdv').text # 등록일자
+        onv = item.find('onv').text # 공개번호
+        odv = item.find('odv').text # 공개일자
+        agv = item.find('agv').text # 대리인
+        inv = item.find('inv').text # 발명자
+        abv = item.find('abv').text # 상세 설명
+        result_list.append(sort(tlv,ipv,apv,vdkvgwkey,adv,gnv,gdv,onv,odv,agv,inv))
+
+    Real_result_list  = []
+    for i in result_list[:row]:
+        i = "\n".join(i)
+        Real_result_list.append(str(i))
+    # 결과 값 txt 파일 저장
+    with open("search_result.txt", "w", encoding='utf8') as f:
+        f.writelines(Real_result_list)
     
 if __name__ == "__main__":
-    search_result(type="patentList", Keyword="AI+그림")
+    search_result(category="patentList", row=30, Keyword="안경")
+    
+    
+
 
