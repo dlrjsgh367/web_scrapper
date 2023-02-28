9d76173ed84f9ff52f510290caa421ccf0d25d06
dlrjsgh367 <dlrjsgh367@gmail.com>
2023-02-28 21:08:43 +0900

    완성함 post형식으로 request 보냄. 검색어와 나오게 할 결과의 수를 입력하면 상세설명으로 특허명, 출원번호, 요약이 txt로 저장됨.

--- main.py
+++ main.py
@@ -1,96 +1,91 @@
 import requests 
 
-from bs4 import BeautifulSoup
-
 import xml
 import xml.etree.ElementTree as ET
-import dotenv
 
-def SSort(x):
-    """
-    xml 태그를 제거해주는 함수입니다.
-    """
-    x_fix = '<root>{}</root>'.format(x)
-    x_content = ET.fromstring(x_fix).text
+from bs4 import BeautifulSoup
 
-    return x_content
 
-def reject_status_remove(status_check):
+
+def reject_status_remove(status_check, title):
     """
-    행정상태 유형이
-    '거절'인 특허를 제거해주는 함수 입니다.
+    status가 '거절'인 것을 제거해주는 함수 입니다.
     """ 
     for status in status_check:
         s = status.find("status").text
         if s == "<![CDATA[거절]]>":
-            print(status)
+            pass
         else:
-            with open('reject_status.xml', "a", encoding="utf8") as f:
+            with open(f'./xml/{title}.xml', "a", encoding="utf8") as f:
                 f.write(str(status))
             
 
-def sort_func(a, d, l):
+def remove_tag(x, y, z):
     """
     xml 태그를 제거해주는 함수입니다.
     """
-    a_fix = '<root>{}</root>'.format(a)
-    a_content = ET.fromstring(a_fix).text
 
-    d_fix = '<root>{}</root>'.format(d)
-    d_content = ET.fromstring(d_fix).text
+    x_fix = f'<root>{x}</root>'
+    x_content = ET.fromstring(x_fix).text
 
-    l_fix = '<root>{}</root>'.format(l)
-    l_content = ET.fromstring(l_fix).text
+    y_fix = f'<root>{y}</root>'
+    y_content = ET.fromstring(y_fix).text
+
+    z_fix = f'<root>{z}</root>'
+    z_content = ET.fromstring(z_fix).text
     
-    result =  f"출원번호 : {d_content}", f"특허명 : {a_content}", f"요약 : {l_content}", "\n"
+    result =  f"출원번호 : {x_content}", f"특허명 : {y_content}", f"요약 : {z_content}", "\n"
     return result
 
-# row : row개의 검색결과를 가져옴
-# Keyword : 검색어 입력(검색어 + 검색어 도 됨)
-def search_result(Keyword, row):
-    # 결과값을 담아놓는 리스트
-    result_list = []
-    
+
+def status_remove(Keyword, row):
+    """
+    Arguments
+    row : row개의 검색결과를 가져옴
+    Keyword : 검색어 입력(검색어 + 검색어 도 됨)  
+    """
+
     # url 
     url = "http://kportal.kipris.or.kr/kportal/resulta.do"
 
     # header
     headers = {
-        "Accept": "application/xml, text/xml, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Connection": "keep-alive",
-        "Content-Length": "117",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": "JSESSIONID=kcz5bnBNFiaICTuFEgysKs4C6S0OISkmhWjAIoRXV2mFcEpbbmLtgmrz93itoRMa.amV1c19kb21haW4va3BvcnRhbDM=; _ga=GA1.3.953350611.1677154053; TM_CONFIG=G11111111111111111SX11111110011111100; AB_CONFIG=G11001111111111111111111111110S10000111000111100001000; AT_CONFIG=G0000000000000S111110110111; KA_CONFIG=G0000000000000S110111000; K2_CONFIG=G1111111111111111111111S111111111000000000; AD_CONFIG=G11111111111111S1111111111111000; _gid=GA1.3.348414736.1677460475; __utmc=31935460; KPAT_RECENTLY_JOB=1020170090723; KD_CONFIG=G1111111111111111111111S111111111000000000; EC=W4SYY9pLzcuGMoQxSBxdG5HMPvt565cyRG7UUK4ffo8=; NKP_CONFIG=G1111111111111111111111S110001000000000000; NAB_CONFIG=G11001111111111111111111111110S10000010000000010000; KP_CONFIG=G1111111111111111111111S111111111000000000; DG_CONFIG=G11111111111111111SX11100110011011111; JM_CONFIG=G11111111111111SX01101111110010; KPAT_SRCH_HISTORY=421CE58A92E2FB07; KP_TOTAL_HISTRY=AI; __utma=31935460.953350611.1677154053.1677475066.1677477746.11; __utmz=31935460.1677477746.11.8.utmcsr=kipris.or.kr|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmt=1; KIPRIS_TODAY_CNT=CHECKED; __utmb=31935460.2.10.1677477746; _gat=1",
-        "Host": "kportal.kipris.or.kr",
-        "Origin": "http://kportal.kipris.or.kr",
-        "Referer": "http://kportal.kipris.or.kr/kportal/search/total_search.do",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest"
-        }
-    
+                "Referer": "http://kportal.kipris.or.kr/kportal/search/total_search.do",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                }
     # data 
     data = {
-    "next": "patentList",
-    "FROM": "SEARCH",
-    "searchInTransKorToEng": "N",
-    "searchInTransEngToKor": "N",
-    "row": f"{row}",
-    "page": "1",
-    "queryText": f"{Keyword}",
-    "expression": f"{Keyword}",
-    }
+            "next": "patentList",
+            "FROM": "SEARCH",
+            "searchInTransKorToEng": "N",
+            "searchInTransEngToKor": "N",
+            "row": f"{row}",
+            "page": "1",
+            "queryText": f"{Keyword}",
+            "expression": f"{Keyword}",
+            }
 
     # response
     res = requests.post(url, headers=headers, data=data)
     
-    soup = BeautifulSoup(res.text, "lxml")
+    soup = BeautifulSoup(res.text, "lxml")    
+    
+    # reject_status_remove arguments
+    status_check = soup.find_all("article")
 
+    # '거절' status 제거 후 xml 파일 저장
+    reject_status_remove(status_check=status_check, title=Keyword)
+
+    # reject_status_remove로 저장한 파일 open
+    result_file = open(f'./xml/{Keyword}.xml', 'r', encoding="utf8")
+
+    soup = BeautifulSoup(result_file, 'lxml')
+
+    # 중간 결과값을 담아놓는 리스트
+    result_list = []
+
+    # find article
     result = soup.find_all("article")
-    with open("./search_result/search_result.xml", "w", encoding='utf8') as f:
-        f.write(str(soup))
-
     for item in result:
         
         # 특허명
@@ -102,63 +97,21 @@ def search_result(Keyword, row):
         # 요약
         abv = item.find('abv').text 
 
-        result_list.append(sort_func(tlv,vdkvgwkey,abv))
+        # xml 태그 제거 후 중간 결과 리스트에 추가
+        result_list.append(remove_tag(tlv,vdkvgwkey,abv))
 
+    # 최종 결과값을 담을 리스트
     Real_result_list  = []
-    for i in result_list[:row]:
+    for i in result_list[:row]: # 0부터 row 만큼 반복
         i = "\n".join(i)
         Real_result_list.append(str(i))
-    # 결과 값 txt 파일 저장
+
+    # 최종 결과 리스트 저장
     with open("./search_result/search_result.txt", "w", encoding='utf8') as f:
         f.writelines(Real_result_list)
 
-def status_remove(Keyword, row):
-    # 결과값을 담아놓는 리스트
-    result_list = []
-    
-    # url 
-    url = "http://kportal.kipris.or.kr/kportal/resulta.do"
-
-    # header
-    headers = {
-        "Accept": "application/xml, text/xml, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Connection": "keep-alive",
-        "Content-Length": "117",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": "JSESSIONID=kcz5bnBNFiaICTuFEgysKs4C6S0OISkmhWjAIoRXV2mFcEpbbmLtgmrz93itoRMa.amV1c19kb21haW4va3BvcnRhbDM=; _ga=GA1.3.953350611.1677154053; TM_CONFIG=G11111111111111111SX11111110011111100; AB_CONFIG=G11001111111111111111111111110S10000111000111100001000; AT_CONFIG=G0000000000000S111110110111; KA_CONFIG=G0000000000000S110111000; K2_CONFIG=G1111111111111111111111S111111111000000000; AD_CONFIG=G11111111111111S1111111111111000; _gid=GA1.3.348414736.1677460475; __utmc=31935460; KPAT_RECENTLY_JOB=1020170090723; KD_CONFIG=G1111111111111111111111S111111111000000000; EC=W4SYY9pLzcuGMoQxSBxdG5HMPvt565cyRG7UUK4ffo8=; NKP_CONFIG=G1111111111111111111111S110001000000000000; NAB_CONFIG=G11001111111111111111111111110S10000010000000010000; KP_CONFIG=G1111111111111111111111S111111111000000000; DG_CONFIG=G11111111111111111SX11100110011011111; JM_CONFIG=G11111111111111SX01101111110010; KPAT_SRCH_HISTORY=421CE58A92E2FB07; KP_TOTAL_HISTRY=AI; __utma=31935460.953350611.1677154053.1677475066.1677477746.11; __utmz=31935460.1677477746.11.8.utmcsr=kipris.or.kr|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmt=1; KIPRIS_TODAY_CNT=CHECKED; __utmb=31935460.2.10.1677477746; _gat=1",
-        "Host": "kportal.kipris.or.kr",
-        "Origin": "http://kportal.kipris.or.kr",
-        "Referer": "http://kportal.kipris.or.kr/kportal/search/total_search.do",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest"
-        }
-    
-    # data 
-    data = {
-    "next": "patentList",
-    "FROM": "SEARCH",
-    "searchInTransKorToEng": "N",
-    "searchInTransEngToKor": "N",
-    "row": f"{row}",
-    "page": "1",
-    "queryText": f"{Keyword}",
-    "expression": f"{Keyword}",
-    }
-
-    # response
-    res = requests.post(url, headers=headers, data=data)
-    
-    soup = BeautifulSoup(res.text, "lxml")    
-    
-    status_check = soup.find_all("article")
-    reject_status_remove(status_check=status_check)
-
-    
-
 if __name__ == "__main__":
-    status_remove(Keyword="양말", row=5)
+    status_remove(Keyword="AI+그림", row=30)
     
     
 
